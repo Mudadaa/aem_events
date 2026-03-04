6bc90125f203c32f798c541f585a731c6479cdfe
Hassan <layeb.hassan@gmail.com>
2026-03-05 00:36:49 +0100

    initialisation projet ajout tailwind et images

---
+++ aem-events/src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <header>
+          
+          <img src="./src/assets/aemeventsLogo.png" alt="logo A et M events"
+          class='border'
+          />
+        </header>
+        
+       </div>
+    </>
+  )
+}
+
+export default App
